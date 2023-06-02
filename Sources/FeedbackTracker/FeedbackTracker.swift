a4d459b16077b87b15adaa7651fdc7e4651a665f
Bagmet Denis <87632908+BagmetDenis@users.noreply.github.com>
2023-06-02 12:54:39 +0300

    +

--- Sources/FeedbackTracker/FeedbackTracker.swift
+++ Sources/FeedbackTracker/FeedbackTracker.swift
@@ -9,21 +9,30 @@ import SwiftUI
 //    }
 //}
 
-public struct FeedBackAlert {
-    @StateObject var feedbackRepository = FeedbackRepository()
-    public func showAlert(completion: @escaping () -> Void) -> TextFieldAlert {
-        let alert = TextFieldAlert(title: "Test", textFields: [
-            .init(text: $feedbackRepository.email, placeholder: "email"),
-            .init(text: $feedbackRepository.message, placeholder: "message"),
-        ], actions: [
-            .init(title: "Cancel"),
-            .init(title: "Submit", closure: { _ in
-                Task{
-                    await feedbackRepository.sendFeedback()
-                }
-            })
-        ], isPresented: .constant(false))
-        
-        return alert
+//public struct FeedBackAlert {
+//    @StateObject var feedbackRepository = FeedbackRepository()
+//    @State var showAlert: Bool = false
+//    public func showAlert(completion: @escaping () -> Void) -> TextFieldAlert {
+//        let alert = TextFieldAlert(title: "Test", textFields: [
+//            .init(text: $feedbackRepository.email, placeholder: "email"),
+//            .init(text: $feedbackRepository.message, placeholder: "message"),
+//        ], actions: [
+//            .init(title: "Cancel"),
+//            .init(title: "Submit", closure: { _ in
+//                Task{
+//                    await feedbackRepository.sendFeedback()
+//                }
+//            })
+//        ], isPresented: $showAlert)
+//
+//        return alert
+//    }
+//}
+
+public extension View{
+    func feedbackAlert(isPresented: Binding<Bool>) -> some View{
+        TextFieldWrapper(isPresented: isPresented, presentingView: self) {
+            TextFieldAlert(title: "Test")
+        }
     }
 }
