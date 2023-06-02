9dd201b7ab94ea603c95b212aa89b1acdca20828
Bagmet Denis <87632908+BagmetDenis@users.noreply.github.com>
2023-06-02 12:33:32 +0300

    +

--- Sources/FeedbackTracker/FeedbackTracker.swift
+++ Sources/FeedbackTracker/FeedbackTracker.swift
@@ -1,6 +1,23 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-public func sendFeedback(){
-    
+import SwiftUI
+
+//public struct FeedBackAlert: View{
+//    @StateObject var feedbackRepository = FeedbackRepository()
+//    public var body: some View{
+//        
+//    }
+//}
+
+public struct FeedBackAlert {
+    public static func showAlert(completion: @escaping () -> Void) -> Alert {
+        let alert = Alert(
+            title: Text("Custom Alert"),
+            message: Text("This is a custom alert."),
+            primaryButton: .default(Text("OK"), action: completion),
+            secondaryButton: .cancel()
+        )
+        return alert
+    }
 }
