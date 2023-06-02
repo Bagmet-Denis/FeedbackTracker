1bb09ef2403c01fba29d63cb814c8d1741b335aa
Bagmet Denis <87632908+BagmetDenis@users.noreply.github.com>
2023-06-02 12:46:12 +0300

    +

--- Sources/FeedbackTracker/FeedbackTracker.swift
+++ Sources/FeedbackTracker/FeedbackTracker.swift
@@ -2,7 +2,6 @@
 // https://docs.swift.org/swift-book
 
 import SwiftUI
-
 //public struct FeedBackAlert: View{
 //    @StateObject var feedbackRepository = FeedbackRepository()
 //    public var body: some View{
@@ -11,13 +10,20 @@ import SwiftUI
 //}
 
 public struct FeedBackAlert {
-    public static func showAlert(completion: @escaping () -> Void) -> Alert {
-        let alert = Alert(
-            title: Text("Custom Alert"),
-            message: Text("This is a custom alert."),
-            primaryButton: .default(Text("OK"), action: completion),
-            secondaryButton: .cancel()
-        )
+    @StateObject var feedbackRepository = FeedbackRepository()
+    public func showAlert(completion: @escaping () -> Void) -> TextFieldAlert {
+        let alert = TextFieldAlert(title: "Test", textFields: [
+            .init(text: $feedbackRepository.email, placeholder: "email"),
+            .init(text: $feedbackRepository.message, placeholder: "message"),
+        ], actions: [
+            .init(title: "Cancel"),
+            .init(title: "Submit", closure: { _ in
+                Task{
+                    await feedbackRepository.sendFeedback()
+                }
+            })
+        ], isPresented: .constant(false))
+        
         return alert
     }
 }
