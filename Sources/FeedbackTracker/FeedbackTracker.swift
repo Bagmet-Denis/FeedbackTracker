0fba9e415e1b47c0c918699604a33653c7aeeb37
Bagmet Denis <87632908+BagmetDenis@users.noreply.github.com>
2023-06-03 11:59:10 +0300

    +

--- Sources/FeedbackTracker/FeedbackTracker.swift
+++ Sources/FeedbackTracker/FeedbackTracker.swift
@@ -2,6 +2,7 @@
 // https://docs.swift.org/swift-book
 
 import SwiftUI
+import MessageUI
 
 public extension View{
     func feedbackAlert(isPresented: Binding<Bool>) -> some View{
@@ -12,9 +13,37 @@ public extension View{
 }
 
 public extension View {
+    
     func addFeedback(isPresented: Binding<Bool>) -> some View {
         self.actionSheet(isPresented: isPresented) {
-            ActionSheet(title: Text("Test"))
+            ActionSheet(title: Text("Feedback"), buttons: [
+                .default(Text("Quick Feedback"), action: {
+                    feedbackAlert(isPresented: isPresented)
+                }),
+                .default(Text("Send to Email"), action: {
+                    sendEmail()
+                }),
+                .cancel()
+            ])
         }
+    }
+    
+    func sendEmail() {
+        let recipient = "info@appbox.pw"
+        let mailComposer = MFMailComposeViewController()
+        mailComposer.setToRecipients([recipient])
+        mailComposer.setSubject("Привет!")
+        mailComposer.setMessageBody("Привет, как дела?", isHTML: false)
+        
+        guard MFMailComposeViewController.canSendMail() else {
+            print("Не удалось отправить письмо.")
+            return
+        }
+        
+        guard let viewController = UIApplication.shared.windows.first?.rootViewController else {
+            return
+        }
+        
+        viewController.present(mailComposer, animated: true)
     }
 }
