e3199af6b94349b68d8d3583e395b956214815b3
Bagmet Denis <87632908+BagmetDenis@users.noreply.github.com>
2023-06-03 12:55:31 +0300

    +

--- Sources/FeedbackTracker/FeedbackTracker.swift
+++ Sources/FeedbackTracker/FeedbackTracker.swift
@@ -12,103 +12,103 @@ public extension View{
     }
 }
 
-public extension View {
-    
-    func addFeedback(isPresented: Binding<Bool>) -> some View {
-        self.actionSheet(isPresented: isPresented) {
-            ActionSheet(title: Text("Feedback"), buttons: [
-                .default(Text("Quick Feedback"), action: {
-                    textFieldAlert(title: "", textFields: [], actions: [], isPresented: isPresented)
-                }),
-                .default(Text("Send to Email"), action: {
-                    sendEmail()
-                }),
-                .cancel()
-            ])
-        }
-    }
-    
-    func sendEmail() {
-        let recipient = "info@appbox.pw"
-        let mailComposer = MFMailComposeViewController()
-        mailComposer.setToRecipients([recipient])
-        mailComposer.setSubject("Привет!")
-        mailComposer.setMessageBody("Привет, как дела?", isHTML: false)
-        
-        guard MFMailComposeViewController.canSendMail() else {
-            print("Не удалось отправить письмо.")
-            return
-        }
-        
-        guard let viewController = UIApplication.shared.windows.first?.rootViewController else {
-            return
-        }
-        
-        viewController.present(mailComposer, animated: true)
-    }
-}
+//public extension View {
+//
+//    func addFeedback(isPresented: Binding<Bool>) -> some View {
+//        self.actionSheet(isPresented: isPresented) {
+//            ActionSheet(title: Text("Feedback"), buttons: [
+//                .default(Text("Quick Feedback"), action: {
+//                    textFieldAlert(title: "", textFields: [], actions: [], isPresented: isPresented)
+//                }),
+//                .default(Text("Send to Email"), action: {
+//                    sendEmail()
+//                }),
+//                .cancel()
+//            ])
+//        }
+//    }
+//
+//    func sendEmail() {
+//        let recipient = "info@appbox.pw"
+//        let mailComposer = MFMailComposeViewController()
+//        mailComposer.setToRecipients([recipient])
+//        mailComposer.setSubject("Привет!")
+//        mailComposer.setMessageBody("Привет, как дела?", isHTML: false)
+//
+//        guard MFMailComposeViewController.canSendMail() else {
+//            print("Не удалось отправить письмо.")
+//            return
+//        }
+//
+//        guard let viewController = UIApplication.shared.windows.first?.rootViewController else {
+//            return
+//        }
+//
+//        viewController.present(mailComposer, animated: true)
+//    }
+//}
+//
+//public extension View {
+//    func customAlertView(isAlertVisible: Binding<Bool>) -> some View {
+//        CustomAlertView(isShowSheet: isAlertVisible)
+//    }
+//}
+//
+//struct CustomAlertView: View {
+//    @Binding var isShowSheet: Bool
+//    @State var showAlert: Bool = false
+//
+//    var body: some View {
+//        EmptyView()
+//            .feedbackAlert(isPresented: $showAlert)
+//            .actionSheet(isPresented: $isShowSheet) {
+//                ActionSheet(title: Text("Feedback"), buttons: [
+//                    .default(Text("Quick Feedback"), action: {
+//                        showAlert = true
+//                    }),
+//                    .default(Text("Send to Email"), action: {
+//                        sendEmail()
+//                    }),
+//                    .cancel()
+//                ])
+//            }
+//    }
+//
+//    func sendEmail() {
+//        let recipient = "info@appbox.pw"
+//        let mailComposer = MFMailComposeViewController()
+//        mailComposer.setToRecipients([recipient])
+//        mailComposer.setSubject("Привет!")
+//        mailComposer.setMessageBody("Привет, как дела?", isHTML: false)
+//
+//        guard MFMailComposeViewController.canSendMail() else {
+//            print("Не удалось отправить письмо.")
+//            return
+//        }
+//
+//        guard let viewController = UIApplication.shared.windows.first?.rootViewController else {
+//            return
+//        }
+//
+//        viewController.present(mailComposer, animated: true)
+//    }
+//}
+
 
 public extension View {
-    func customAlertView(isAlertVisible: Binding<Bool>) -> some View {
-        CustomAlertView(isShowSheet: isAlertVisible)
-    }
-}
-
-struct CustomAlertView: View {
-    @Binding var isShowSheet: Bool
-    @State var showAlert: Bool = false
-    
-    var body: some View {
-        EmptyView()
-            .feedbackAlert(isPresented: $showAlert)
-            .actionSheet(isPresented: $isShowSheet) {
-                ActionSheet(title: Text("Feedback"), buttons: [
-                    .default(Text("Quick Feedback"), action: {
-                        showAlert = true
-                    }),
-                    .default(Text("Send to Email"), action: {
-                        sendEmail()
-                    }),
-                    .cancel()
-                ])
-            }
-    }
-    
-    func sendEmail() {
-        let recipient = "info@appbox.pw"
-        let mailComposer = MFMailComposeViewController()
-        mailComposer.setToRecipients([recipient])
-        mailComposer.setSubject("Привет!")
-        mailComposer.setMessageBody("Привет, как дела?", isHTML: false)
-        
-        guard MFMailComposeViewController.canSendMail() else {
-            print("Не удалось отправить письмо.")
-            return
-        }
-        
-        guard let viewController = UIApplication.shared.windows.first?.rootViewController else {
-            return
-        }
-        
-        viewController.present(mailComposer, animated: true)
-    }
-}
-
-
-public extension View {
-    func content<Content: View>(@ViewBuilder content: () -> Content, showSheet: Binding<Bool>) -> some View {
-        self.modifier(ContentModifier(showSheet: showSheet))
+    func addFeedback(isPresented: Binding<Bool>) -> some View{
+        modifier(ContentModifier(isPresented: isPresented))
     }
 }
 
 struct ContentModifier: ViewModifier {
-    @Binding var showSheet: Bool
+    @Binding var isPresented: Bool
     @State var showAlert: Bool = false
     
     func body(content: Content) -> some View {
         content
             .feedbackAlert(isPresented: $showAlert)
-            .actionSheet(isPresented: $showSheet) {
+            .actionSheet(isPresented: $isPresented) {
                 ActionSheet(title: Text("Feedback"), buttons: [
                     .default(Text("Quick Feedback"), action: {
                         showAlert = true
