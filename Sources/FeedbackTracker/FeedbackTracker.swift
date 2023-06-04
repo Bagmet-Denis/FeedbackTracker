3278aecd9d9c004f1b2291f44fa14340d2a1787d
Bagmet Denis <87632908+BagmetDenis@users.noreply.github.com>
2023-06-04 17:32:43 +0300

    - test

--- Sources/FeedbackTracker/FeedbackTracker.swift
+++ Sources/FeedbackTracker/FeedbackTracker.swift
@@ -50,6 +50,7 @@ struct FeedbackModifier: ViewModifier {
                     message: $feedbackRepository.message,
                     title: Localization.text(.feedback, language: language)
                 )
+                .ignoresSafeArea()
             }
         }
     }
