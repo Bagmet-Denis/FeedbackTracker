5803b2e69c45c43b76eb6f21093403622aa53952
Bagmet Denis <87632908+BagmetDenis@users.noreply.github.com>
2023-06-04 17:28:54 +0300

    - test

--- Sources/FeedbackTracker/FeedbackTracker.swift
+++ Sources/FeedbackTracker/FeedbackTracker.swift
@@ -4,13 +4,13 @@
 import SwiftUI
 import MessageUI
 
-public extension View{
-    func feedbackAlert(isPresented: Binding<Bool>, language: Language, urlServer: String) -> some View{
-        TextFieldWrapper(isPresented: isPresented, urlServer: urlServer, language: language, presentingView: self) {
-            TextFieldAlert(title: "test")
-        }
-    }
-}
+//public extension View{
+//    func feedbackAlert(isPresented: Binding<Bool>, language: Language, urlServer: String) -> some View{
+//        TextFieldWrapper(isPresented: isPresented, urlServer: urlServer, language: language, presentingView: self) {
+//            TextFieldAlert(title: "test")
+//        }
+//    }
+//}
 
 public extension View {
     func addFeedback(isPresented: Binding<Bool>, language: Language, urlServer: String) -> some View{
@@ -19,6 +19,7 @@ public extension View {
 }
 
 struct FeedbackModifier: ViewModifier {
+    @StateObject var feedbackRepository = FeedbackRepository()
     @Binding var isPresented: Bool
     let language: Language
     let urlServer: String
@@ -27,7 +28,6 @@ struct FeedbackModifier: ViewModifier {
     
     func body(content: Content) -> some View {
         content
-            .feedbackAlert(isPresented: $showAlert, language: language, urlServer: urlServer)
             .actionSheet(isPresented: $isPresented) {
                 ActionSheet(title: Text(Localization.text(.titleSheet, language: language)), buttons: [
                     .default(Text(Localization.text(.quickFeedback, language: language)), action: {
@@ -39,6 +39,17 @@ struct FeedbackModifier: ViewModifier {
                     .cancel(Text(Localization.text(.cancel, language: language)))
                 ])
             }
+        
+        if showAlert{
+            FeedbackAlertView(
+                isPresented: $showAlert,
+                emailPlaceholder: Localization.text(.email, language: language),
+                email: $feedbackRepository.email,
+                messagePlaceholder: Localization.text(.message, language: language),
+                message: $feedbackRepository.message,
+                title: Localization.text(.feedback, language: language)
+            )
+        }
     }
     
     func openMail() {
@@ -46,6 +57,117 @@ struct FeedbackModifier: ViewModifier {
            UIApplication.shared.canOpenURL(url)
         {
             UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
+}
+
+struct FeedbackAlertView: View {
+    @Binding var isPresented: Bool
+    
+    var emailPlaceholder: String = ""
+    @Binding var email: String
+    
+    var messagePlaceholder: String = ""
+    @Binding var message: String
+    
+    let title: String
+    
+    var body: some View {
+        ZStack {
+            if isPresented {
+                Color.gray.opacity(0.3).edgesIgnoringSafeArea(.all)
+                    .onTapGesture {
+                        isPresented = false
+                    }
+                
+                VStack(spacing: 0) {
+                    Text(title)
+                        .font(.system(size: 18, weight: .bold))
+                        .padding()
+                    
+                    Divider()
+                    
+                    TextField(emailPlaceholder, text: $email)
+                        .textFieldStyle(.plain)
+                        .frame(height: 40)
+                        .padding(.horizontal, 5)
+                        .background(Color.white)
+                    
+                    Divider()
+                    
+                    CustomFeedbackTextEditor(placeholder: messagePlaceholder, text: $message)
+                    
+                    Divider()
+                    
+                    HStack{
+                        Button {
+                            isPresented = false
+                        } label: {
+                            HStack{
+                                Spacer(minLength: 0)
+                                
+                                Text("Отмена")
+                                
+                                Spacer(minLength: 0)
+                            }
+                            .padding(.vertical, 15)
+                            .contentShape(Rectangle())
+                        }
+                        
+                        Divider()
+                            .frame(height: 50)
+                        
+                        Button {
+                            
+                        } label: {
+                            HStack{
+                                Spacer(minLength: 0)
+                                
+                                Text("Отправить")
+                                
+                                Spacer(minLength: 0)
+                            }
+                            .padding(.vertical, 15)
+                            .contentShape(Rectangle())
+                        }
+                    }
+                }
+                .frame(width: UIScreen.main.bounds.width / 1.5)
+                .background(Color(UIColor.systemGray6))
+                .cornerRadius(15)
+                .onAppear {
+                    UITextView.appearance().backgroundColor = .clear
+                }
+            }
+        }
+    }
+}
+
+struct CustomFeedbackTextEditor: View {
+    var placeholder: String = "Denis"
+    @Binding var text: String
+    
+    let internalPadding: CGFloat = 5
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            if text.isEmpty  {
+                Text(placeholder)
+                    .foregroundColor(Color.primary.opacity(0.25))
+                    .padding(EdgeInsets(top: 7, leading: 4, bottom: 0, trailing: 0))
+                    .padding(internalPadding)
+            }
+            
+            TextEditor(text: $text)
+                .padding(internalPadding)
+                .frame(height: UIScreen.main.bounds.height / 7)
+                .background(Color.white)
+                .cornerRadius(5)
+                
+        }.onAppear() {
+            UITextView.appearance().backgroundColor = .clear
+        }.onDisappear() {
+            UITextView.appearance().backgroundColor = nil
         }
     }
 }
