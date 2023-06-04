79f19c20faffdb224d43050f1034dda97e6e48fb
Bagmet Denis <87632908+BagmetDenis@users.noreply.github.com>
2023-06-04 13:55:01 +0300

    - test

--- Sources/FeedbackTracker/TextFieldAlertViewController.swift
+++ Sources/FeedbackTracker/TextFieldAlertViewController.swift
@@ -112,16 +112,19 @@ public final class TextFieldAlertViewController: UIViewController {
         
         // Создаем пользовательский контроллер представления
         let customViewController = UIViewController()
-//        customViewController.view.translatesAutoresizingMaskIntoConstraints = false
-//        customViewController.view.backgroundColor = .red
+        
+        // Создаем и настраиваем UITextField
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.backgroundColor = .white
+        textField.placeholder = "Email"
+        textField.font = UIFont.systemFont(ofSize: 14)
+        customViewController.view.addSubview(textField)
 
         // Create a UITextView
         let textView = UITextView()
         textView.translatesAutoresizingMaskIntoConstraints = false
         textView.backgroundColor = .clear
-        textView.layer.borderWidth = 1.0
-        textView.layer.borderColor = UIColor.gray.cgColor
-        textView.layer.cornerRadius = 6.0
         customViewController.view.addSubview(textView)
 
         // Add constraints to make the textView occupy the available width
@@ -133,14 +136,6 @@ public final class TextFieldAlertViewController: UIViewController {
         
         // Добавляем пользовательский контроллер представления в UIAlertController
         alertController.setValue(customViewController, forKey: "contentViewController")
-        
-//        // Создаем и настраиваем UITextField
-//        let textField = UITextField()
-//        textField.translatesAutoresizingMaskIntoConstraints = false
-//        textField.backgroundColor = .white
-//        textField.placeholder = "Email"
-//        textField.font = UIFont.systemFont(ofSize: 14)
-//        customViewController.view.addSubview(textField)
 //
 //        // Создаем и настраиваем UITextView
 //        let textView = UITextView()
