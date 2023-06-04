5bdfdff2b91deafa6c6d17aac9fd6880daa75d56
Bagmet Denis <87632908+BagmetDenis@users.noreply.github.com>
2023-06-04 13:24:03 +0300

    - test

--- Sources/FeedbackTracker/TextFieldAlertViewController.swift
+++ Sources/FeedbackTracker/TextFieldAlertViewController.swift
@@ -113,7 +113,7 @@ public final class TextFieldAlertViewController: UIViewController {
         // Создаем пользовательский контроллер представления
         let customViewController = UIViewController()
         customViewController.view.translatesAutoresizingMaskIntoConstraints = false
-        customViewController.view.backgroundColor = .clear
+        customViewController.view.backgroundColor = .red
         
         // Создаем и настраиваем UITextField
         let textField = UITextField()
